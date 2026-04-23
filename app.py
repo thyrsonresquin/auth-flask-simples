fde826c61b1f2e7245dc7ababbb3efc3a9a77575
thyrsonresquin <thyrson@outlook.com>
2026-04-22 21:36:09 -0400

    teste post

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 # session <- conexão ativa com o banco de dados
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        return jsonify({'message': 'login bem-sucedido'}), 200
+    else:
+        return jsonify({'message': 'credenciais invalidas'}), 400
+    
 
 @app.route('/hello', methods=['GET'])
 def hello():
